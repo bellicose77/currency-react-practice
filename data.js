05538a447e854531458073524f652c484ed28bf3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-14 01:19:05 +0600

    data file

---
+++ data.js
@@ -0,0 +1,38 @@
+export const currencyPositionData2 = [
+    {
+      id: 1,
+      name: 'kr 1,000,00',
+      value: 'left_with_space',
+      isActive: false,
+      fieldName: 'currencyPosition'
+    },
+    {
+      id: 2,
+      name: '1,000,00 kr',
+      value: 'right_with_space',
+      isActive: false,
+      fieldName: 'currencyPosition'
+    },
+    { id: 3, name: 'kr1,000,00', value: 'left', isActive: false, fieldName: 'currencyPosition' },
+    { id: 4, name: '1,000,00kr', value: 'right', isActive: true, fieldName: 'currencyPosition' }
+  ];
+  
+  export const currencyPositionsData = [
+    {
+      id: 1,
+      name: 'kr 1,000',
+      value: 'left_with_space',
+      isActive: false,
+      fieldName: 'currencyPosition'
+    },
+    {
+      id: 2,
+      name: '1,000 kr',
+      value: 'right_with_space',
+      isActive: false,
+      fieldName: 'currencyPosition'
+    },
+    { id: 3, name: 'kr1,000', value: 'left', isActive: false, fieldName: 'currencyPosition' },
+    
+    { id: 4, name: '1,000kr', value: 'right', isActive: true, fieldName: 'currencyPosition' }
+  ];
